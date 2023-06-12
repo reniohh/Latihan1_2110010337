5438cac7d9943ef3dd39ac875720f7d8a074b27f
reniohh <renohhreni@gmail.com>
2023-06-12 21:35:36 +0800

    membuat kelas matematikaberaksi

--- src/hitung/Matematikaberaksi.java
+++ src/hitung/Matematikaberaksi.java
@@ -1,7 +1,7 @@
 
 package hitung;
 
-public class MatematikaBeraksi {
+public class Matematikaberaksi {
     public static void main(String[] args) {
     //membuat objek
     Matematika reni = new Matematika (3,7);
@@ -11,4 +11,4 @@ public class MatematikaBeraksi {
     System.out.println("Hasil perkalian   : "+reni.setPerkalian());
     System.out.println("Hasil pembagian   : "+reni.setPembagian());
     }
-    }
+} 
